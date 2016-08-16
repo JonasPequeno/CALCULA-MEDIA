7779d68ae5b7debd08bb87444729ea5d1f76838e
Lyndemberg Batista <lyndemberg_batista@hotmail.com>
2016-08-16 16:25:00 -0300

    Iniciando uma nova funcionalidade

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 buttonCalcular.onclick=function(){
     var media=parseFloat(nota1.value.replace(',','.'))+parseFloat(nota2.value.replace(',','.'))+parseFloat(nota3.value.replace(',','.'));
     var mediaGeral=media/3;
-    alert(mediaGeral);   
+    resultado.innerHTML=mediaGeral;   
 
 }
 buttonLimpar.onclick=function(){
