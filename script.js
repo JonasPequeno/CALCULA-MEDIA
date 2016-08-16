fb0d51859bf2c22b2d503369b8aca716a2ed5c7f
Lyndemberg Batista <lyndemberg_batista@hotmail.com>
2016-08-16 16:50:14 -0300

    EXIBINDO A MENSAGEM DENTRO DO HTML

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 buttonCalcular.onclick=function(){
     var media=parseFloat(nota1.value.replace(',','.'))+parseFloat(nota2.value.replace(',','.'))+parseFloat(nota3.value.replace(',','.'));
     var mediaGeral=media/3;
-    resultado.innerHTML=mediaGeral;   
+    if (mediaGeral<4){
+        resultado.innerHTML="Reprovado! Sua média é "+mediaGeral;  
+    }
+    if (mediaGeral>=4 && mediaGeral<7){
+        var falta=(50-6*mediaGeral)/4;
+        resultado.innerHTML="Você está na final! Sua média é "+mediaGeral+" e precisará de "+falta+" na final";
+    }
+    if(mediaGeral>=7){
+        resultado.innerHTML="Aprovado! Sua média é "+mediaGeral;
+    }
+       
 
 }
 buttonLimpar.onclick=function(){
